0fdd82b49661053102749c7d2dd6b8b638365cf5
Nick <nick58@gmail.com>
2009-03-06 21:15:07 -0700

    updated assembly info

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Little Apps (http://www.littleapps.co.cc)")]
 [assembly: AssemblyProduct("Little Registry Cleaner")]
-[assembly: AssemblyCopyright("Copyright © Little Apps 2008")]
+[assembly: AssemblyCopyright("Copyright © Little Apps 2008-2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
