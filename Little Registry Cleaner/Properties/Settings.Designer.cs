d5b98ea9e05d5a9d6ac130fbcb921fc3da18858c
little-apps <nick@little-apps.com>
2009-11-20 00:43:38 -0700

    final build (1.4)

--- Little Registry Cleaner/Properties/Settings.Designer.cs
+++ Little Registry Cleaner/Properties/Settings.Designer.cs
@@ -175,7 +175,7 @@ namespace Little_Registry_Cleaner.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool bPortableEdition {
             get {
                 return ((bool)(this["bPortableEdition"]));
